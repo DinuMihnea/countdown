cd654af7c02aa1b315c5ca47015131d46b02c23b
Dinu Mihnea <dinoom97@gmail.com>
2018-04-01 15:59:45 +0300

    Improve toolbar styles

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,19 +1,21 @@
 <template>
   <header class="toolbar">
-    <div class="left-tools">
-      <div class="toolbar-group">
-        <div class="item"
-             v-for="item in languages"
-             :key="item.lang"
-             @click="switchLocale(item)"
-             :title="item.name"
-             :class="item.isActive ? 'active' : ''"
-        >{{ item.lang }}</div>
+    <div class="left toolbar-group">
+      <div class="item small"
+           v-for="item in languages"
+           :key="item.lang"
+           @click="switchLocale(item)"
+           :title="item.name"
+           :class="item.isActive ? 'active' : ''"
+      >
+        <span>{{ item.lang }}</span>
       </div>
     </div>
-    <div class="right-tools">
-      <div class="toolbar-group">
-        <div class="item">{{ $t("messages.toolbar.aboutUs") }}</div>
+    <div class="right toolbar-group">
+      <div class="">
+        <div class="item">
+          <span>{{ $t("messages.toolbar.aboutUs") }}</span>
+        </div>
       </div>
     </div>
   </header>
@@ -71,33 +73,46 @@
     left: 0
     box-sizing: border-box
     border-bottom: $border
-    .left-tools
-      float: left
+    height: 64px
+    display: flex
+    align-items: center
+    justify-content: space-between
+    .toolbar-group
+      display: flex
       height: 100%
-      border-right: $border
-    .right-tools
-      float: right
-      height: 100%
-      border-left: $border
+      flex-direction: row
+      &.left
+        border-right: $border
+      &.right
+        border-left: $border
     .item
+      display: flex
+      align-items: center
+      justify-content: center
       color: $white
       height: 100%
-      padding: 20px 20px
-      display: flex
+      padding: 0 20px
       text-transform: uppercase
       font-size: $font-size--normal
       font-weight: 300
       cursor: pointer
       transition: background-color .2s
       opacity: .85
+      span
+        height: $font-size--normal
+        line-height: $font-size--normal
+      &.small
+        font-size: $font-size--small
+        padding: 0 12px
+        span
+          height: $font-size--small
+          line-height: $font-size--small
       &.active
         font-weight: 500
         opacity: 1
-      &:hover
+        cursor: default
+      &:hover:not(.active)
         background-color: rgba($accent, .4)
-    .toolbar-group
-      display: flex
-      flex-direction: row
       .item:not(:first-child)
         border-left: $border
 </style>
